4c93aeb12c910299aca4a91ad554834143a65a52
Maxime Franco <franco.maxime@gmail.com>
2020-02-03 10:39:24 +0100

    serial to pep8

--- serial2.py
+++ serial2.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-import os
 import serial
 import datetime
 import time
 import signal
-import sys
 
 datetimeformat = "%Y-%m-%d_%H:%M:%S"
+
 
 def timestamp_to_date(now):
     """Fonction qui transforme une date de format <timestamp POSIX> en format <datetimeformat> (declare a la ligne 4).
@@ -20,37 +19,39 @@ def timestamp_to_date(now):
 
     """
     return datetime.datetime.fromtimestamp(int(now)).strftime(datetimeformat)
+
+
 def signal_handler(sig, frame):
-	print("ctrl-c catch")
-	global working
-	working = False
-
-DATE=timestamp_to_date(time.time())
-working = True
-signal.signal(signal.SIGINT, signal_handler)
-with open(DATE+".txt","w")as file:
-
-	#usb = os.system('ls /dev/ | grep ttyU')
-	#print(str(usb))
-	texte=input("entrez nom, repas et boisson \n")
-	file.write(texte+'\n')
-	ser = serial.Serial(
-	 port='/dev/ttyUSB0',
-	 baudrate = 9600,
-	 parity=serial.PARITY_NONE,
-	 stopbits=serial.STOPBITS_ONE,
-	 bytesize=serial.EIGHTBITS,
-	 timeout=1
-	)
-	counter=0
+    print("ctrl-c catch")
+    global working
+    working = False
 
 
+if __name__ == "__main__":
+    DATE = timestamp_to_date(time.time())
+    working = True
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTSTP, signal_handler)
 
-	while working:
-	    x=ser.readline()
-	    print(x.decode('utf-8'))
-	    file.write(x.decode('utf-8'))
+    with open(DATE + ".txt", "w")as file:
+        # usb = os.system('ls /dev/ | grep ttyU')
+        # print(str(usb))
+        texte = input("entrez nom, repas et boisson \n")
+        file.write(texte + '\n')
+        ser = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+        counter = 0
 
+        while working:
+            x = ser.readline()
+            print(x.decode('utf-8'))
+            file.write(x.decode('utf-8'))
 
-if working == False:
-	print("exit")
+    if not working:
+        print("exit")
